bb147629d49c640a6077b7a2a0d6634a7e0ad0a8
aadil42 <adilkelawala@gmail.com>
2023-05-10 15:44:45 +0530

    handling network error with try and catch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const [dummyMovies, setDummyMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [error, setError] = useState(false);
   // const fetchMovies = () => {
   //   fetch('https://swapi.dev/api/films/').then((data) => {
   //     return data.json();
@@ -30,19 +30,28 @@ function App() {
    const fetchMovies = async  () => {
     setIsLoading(true);
     let response = await fetch('https://swapi.dev/api/films/');
-    response = await response.json();
-    const newData = response.results.map((singleData) => {
-      return {
-        id: singleData.episode_id,
-        title: singleData.title,
-        releaseDate: singleData.release_date,
-        openingText: singleData.opening_crawl
-      }
-    });
+
+    try {
+      response = await response.json();
+      const newData = response.results.map((singleData) => {
+        return {
+          id: singleData.episode_id,
+          title: singleData.title,
+          releaseDate: singleData.release_date,
+          openingText: singleData.opening_crawl
+        }
+      });
+
+      setDummyMovies(() => {
+        return newData;
+      });
+
+    } catch {
+      // console.log('hehe');
+      setError(true);
+    }
+
     setIsLoading(false);
-    setDummyMovies(() => {
-      return newData;
-    });
   }
 
   return (
@@ -51,9 +60,11 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && dummyMovies.length > 0 &&  <MoviesList movies={dummyMovies} />}
-        {!isLoading && dummyMovies.length === 0 && <p>No Movies Found :(</p>}
-        {isLoading && <p>Loading...</p>}
+        {!isLoading && dummyMovies.length > 0 && !error &&  <MoviesList movies={dummyMovies} />}
+        {!isLoading && error && <p>Network Error</p>}
+        {!isLoading && dummyMovies.length === 0 && !error && <p>No Movies Found :(</p>}
+        {isLoading && !error && <p>Loading...</p>}
+        
       </section>
     </React.Fragment>
   );
